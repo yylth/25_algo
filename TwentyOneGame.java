ce3092a34b514c2d8d46a3f7352c1b2541428b55
yylth <yt2503@y-y.hs.kr>
2025-11-06 15:59:48 +0900

    배스킨라빈스31

--- TwentyOneGame.java
+++ TwentyOneGame.java
@@ -10,19 +10,19 @@ public class TwentyOneGame {
         int computer=0;
         boolean Turn = true; // true면 사용자 차례, false면 컴퓨터 차례
 
-        System.out.println("              21 게임 시작!");
-        while (Num < 21) {
+        System.out.println("              배스킨라빈스 31 시작!");
+        while (Num < 31) {
             if (Turn) {
                 human = 0;
-                while (human != 1 && human != 2) {
-                    System.out.print("              몇 개의 숫자를 말할까요? (1 또는 2 입력): ");
+                while (human != 1 && human != 2 && human != 3) {
+                    System.out.print("              몇 개의 숫자를 말할까요? (1 ~ 3): ");
                     human = in.nextInt();
                 }
                 for (int i = 0; i < human; i++) {
                     Num++;
                     System.out.println(" 사람:\t" + Num);
-                    if (Num == 21) {
-                        System.out.println("              사람이 21을 말했습니다. 컴퓨터 승!");
+                    if (Num == 31) {
+                        System.out.println("              사람이 31을 말했습니다. 컴퓨터 승!");
                         in.close();
                         return;
                     }
@@ -30,13 +30,13 @@ public class TwentyOneGame {
                 Turn = false;
             } else {
                 Random rand = new Random();
-                computer = rand.nextInt(2) + 1;
+                computer = rand.nextInt(3) + 1;
             	System.out.println();
                 for (int i = 0; i < computer; i++) {
                     Num++;
                     System.out.println("컴퓨터:\t" + Num);
-                    if (Num == 21) {
-                        System.out.println("              컴퓨터가 21을 말했습니다. 사람 승!");
+                    if (Num == 31) {
+                        System.out.println("              컴퓨터가 31을 말했습니다. 사람 승!");
                         in.close();
                         return;
                     }
